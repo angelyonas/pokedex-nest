6daf6d5188383bde6f02ba393782bf5c8ab18256
jonathan Arredondo <jonawork94@gmail.com>
2023-08-02 17:38:18 -0600

    Sección 7 terminada

---
+++ src/pokemon/pokemon.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PokemonService } from './pokemon.service';
+import { PokemonController } from './pokemon.controller';
+import { PockemonSchema, Pokemon } from './entities/pokemon.entity';
+
+@Module({
+  controllers: [PokemonController],
+  providers: [PokemonService],
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Pokemon.name,
+        schema: PockemonSchema,
+      },
+    ]),
+  ],
+})
+export class PokemonModule {}
